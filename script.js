283f64d9d1a45c24cf22c3f04ed28a5bd0668228
sky-git9 <117715724+sky-git9@users.noreply.github.com>
2022-12-17 15:47:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// - Noel Delgado | @pixelia_me
+
+const nodes = [].slice.call(document.querySelectorAll('li'), 0);
+const directions = { 0: 'top', 1: 'right', 2: 'bottom', 3: 'left' };
+const classNames = ['in', 'out'].map(p => Object.values(directions).map(d => `${p}-${d}`)).reduce((a, b) => a.concat(b));
+
+const getDirectionKey = (ev, node) => {
+  const { width, height, top, left } = node.getBoundingClientRect();
+  const l = ev.pageX - (left + window.pageXOffset);
+  const t = ev.pageY - (top + window.pageYOffset);
+  const x = l - width / 2 * (width > height ? height / width : 1);
+  const y = t - height / 2 * (height > width ? width / height : 1);
+  return Math.round(Math.atan2(y, x) / 1.57079633 + 5) % 4;
+};
+
+class Item {
+  constructor(element) {
+    this.element = element;
+    this.element.addEventListener('mouseover', ev => this.update(ev, 'in'));
+    this.element.addEventListener('mouseout', ev => this.update(ev, 'out'));
+  }
+
+  update(ev, prefix) {
+    this.element.classList.remove(...classNames);
+    this.element.classList.add(`${prefix}-${directions[getDirectionKey(ev, this.element)]}`);
+  }
+}
+
+
+nodes.forEach(node => new Item(node));
+
+
+
+
+m
